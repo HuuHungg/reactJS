766b0d33ad179ec83274b58a7c42a2e3ff32210e
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-21 17:35:40 +0700

    add file modifile

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-let root = document.getElementById('root')
-
-let h1Element = document.createElement('h1') 
-h1Element.innerText =  ' ITは難しいですが将来ですからがんばてください'
-h1Element.className = 'test class-2'
-h1Element.id = 'reactId'
-Object.assign(h1Element.style, {
-    color: 'blue',
-    backgroundColor: 'gray'
-})
-
-
-root.appendChild(h1Element)
-
 // DOM
 
-let h2DOM =    document.createElement('h2')
-
-h2DOM.innerText = '英語を勉強して日本語を勉強してITを勉強しています'
-h2DOM.style.color = 'red'
-h2DOM.className = 'Nihon'
-
-document.body.appendChild(h2DOM)
+let ulDOM = document.createElement('ul')
+ulDOM.className = 'file'
+ulDOM.style = 'color: red; font-size: 20px'
 
 
-// React
-
-let reactDOM = React.createElement('h4', {
-    title: 'Hello',
-    className: 'fine'
-}, 'でもITは難しいですか')
+let liDOM1 = document.createElement('li')
+liDOM1.innerText = '日本語を勉強してる'
 
 
-console.log(reactDOM)
+let liDOM2 = document.createElement('li')
+liDOM2.innerText = 'ITを勉強して英語を勉強してる'
+liDOM2.style = 'color: green'
+
+ulDOM.appendChild(liDOM1)
+ulDOM.appendChild(liDOM2)
+
+document.body.appendChild(ulDOM)
+
+
+
+//React
+
+
+let ulReact = React.createElement(
+    'ul',
+    {
+        id: 'name',
+
+    }, 
+     React.createElement ('li', {class: 'react', id: 'Name'}, 'Javascript'),
+     React.createElement ('li', null, 'ReactJS')
+
+    )
+
+console.log(ulReact)
