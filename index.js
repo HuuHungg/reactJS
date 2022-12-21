30704bb623f4e2233f75995f749cbd33e83951b5
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-21 18:14:20 +0700

    add file modifle

--- index.js
+++ index.js
@@ -35,3 +35,37 @@ let ulReact = React.createElement(
     )
 
 console.log(ulReact)
+
+
+// DOM
+let divDom = document.createElement('div')
+divDom.className = 'post-Items'
+
+let h2DOM = document.createElement('h2')
+h2DOM.innerText = '今日は11時に起きました体が悪いですから。こぜん彼女といしょうに飲みました'
+h2DOM.style = 'color: gray'
+h2DOM.title = 'Hoc ReactJS form zero to Hero'
+
+let pDOM = document.createElement('p')
+pDOM.innerText = '明日5時に起くつもりです。今朝ITを勉強して英語を勉強して。がんぱってください'
+
+
+divDom.appendChild(h2DOM)
+divDom.appendChild(pDOM)
+
+document.body.appendChild(divDom)
+
+
+// Reat //
+
+let divReact = React.createElement(
+    'div',
+    {
+        className: 'Post-Items'
+    }, 
+    React.createElement('h2', {title: 'Hoc React at f8'}, 'Hoc ReactJS'),
+    React.createElement('p', null, 'learn ReactJS for zero to hero')
+)
+
+console.log(divReact)
+
