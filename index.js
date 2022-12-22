99ef698c9abbc559df91331e274cb1e600e224ec
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-22 15:33:30 +0700

    add file React

--- index.js
+++ index.js
@@ -1,71 +1,43 @@
-let container = document.getElementById('root')
 
-let divReact = React.createElement(
-    'div',
-    {
-        className: 'Post-Items'
-    }, 
-    React.createElement('h2', {title: 'Hoc React at f8', color: 'red'}, 'ITを勉強してる'),
-    React.createElement('p', null, 'がんぱってください')
+// function Conponent
+
+function Header() {
+    return (
+        <div className="header"> New Header</div>
+    )
+}
+
+function Content() {
+    return (
+        <div className= "content"> ITを勉強してる </div>
+    )
+}
+
+// class
+
+class ClassName extends React.Component {
+    render() {
+        return (
+            <div className="footer">でも難しいです</div>
+        )
+    }
+}
+
+
+
+const app = (
+        <div class="wrapper">
+                <Header />
+                <Content />
+                <ClassName />
+        </div>  
 )
 
-    let root = ReactDOM.createRoot(container)
-    root.render(divReact)
+console.log(app)
 
 
-
-
-
-// React h1
-let root2 = document.getElementById('root2')
-
-let h1React = React.createElement(
-    'h1', 
-    {
-        title: 'hello',
-        className: 'heading',
-    },
-    'Hello guys'
-)
-
-let h1 = ReactDOM.createRoot(root2)
-h1.render(h1React)
-
-
-let ulDom = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', {}, 'Javascript'),
-    React.createElement('li', {}, 'ReactJS')
-)
-
-let ul = ReactDOM.createRoot(root3)
-ul.render(ulDom)
-
-
-// const ul = <ul>
-//     <li>Javascript</li>
-//     <li>ReactJs</li>
-//     <li>NodeJS bassic</li>
-// </ul>
-
-// let ulReact = ReactDOM.createRoot(root4)
-// ulReact.render(ul)
-
-
-// const course = [
-//     {
-//         name: HTMLAllCollection.css,
-//     },
-//     {
-//         name: 'Responsive web design'
-//     },
-//     {
-//         name: 'ReactJS'
-//     }
-// ]
-
-// let ulCourse = ReactDOM.createRoot(root4)
-// ulCourse.render(course)
+let root = document.getElementById('root')
+let list = ReactDOM.createRoot(root)
+list.render(app)
 
 
