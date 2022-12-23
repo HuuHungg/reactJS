9dbd1f7a89fd0e6f8e8ccd99c67e322f9f612208
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-23 12:16:15 +0700

    add file react js

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-function Input ({ label, ...inputProps }) {
+// function Input ({ label, ...inputProps }) {
+//     return (
+//         <div>
+//             <label> {label} </label>
+//             <input {...inputProps}  />
+//         </div>
+//     )
+// }
+
+
+// function App() {
+
+//     return (
+//         <div id="wrapper"> 
+//                 <Input
+//                     label="Full name"
+//                     type = "text"
+//                     placeholder="Enter name..."
+//                     title = "Day la input"
+//                     onFocus = {()=> {
+//                         console.log(Math.random())
+//                     }}
+//                 />
+
+//         </div>
+//     )
+// }
+
+
+
+function List({data, children}) {
+    
     return (
-        <div>
-            <label> {label} </label>
-            <input {...inputProps}  />
-        </div>
+        <ul>
+            {data.map((...props) => children(...props))}
+        </ul>
     )
 }
-
 
 
 function App() {
-
+    const cars = ['BWM', 'Honda', 'Mazda']
+    
     return (
-        <div id="wrapper"> 
-                <Input
-                    label="Full name"
-                    type = "text"
-                    placeholder="Enter name..."
-                    title = "Day la input"
-                    onFocus = {()=> {
-                        console.log(Math.random())
-                    }}
-                />
-
+        <div id="wrapper">
+                <List  data = {cars}>
+                    {(item,index) => <li key={index}>{item}</li>}    
+                 </List>        
         </div>
+
     )
 }
+
 
 ReactDOM.render(<App  />, document.getElementById('root'))
 
