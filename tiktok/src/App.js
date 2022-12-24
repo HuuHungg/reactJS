3cc9e7dc8d7284736dbde82894f0bac250a9a7ff
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-24 16:26:32 +0700

    add file App.js

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,34 +1,64 @@
 import { toBeInTheDOM } from '@testing-library/jest-dom/dist/matchers'
 import { useState } from 'react'
 
-const gifts = [
-  'CPU I9',
-  'MACBOOK AIR M1',
-  'Keychorn k4',
-  'ReactJs',
-  'logitech'
+
+const courses = [
+  {
+    id: 1,
+    name: '日本語',
+  },
+  {
+    id: 2,
+    name: '英語'
+  },
+  {
+    id: 3,
+    name: 'ITを勉強してる'
+  },
+  {
+    id: 4,
+    name: 'ベトナム語'
+  }
 ]
 
 
 function App() {
 
-  const [gift, setGift] = useState()
-  
-  const  handleGift = () => {
-      const index = Math.floor(Math.random()* gifts.length)
-      
-      setGift(gifts[index])
+    const [checked, setCheck] = useState([])
 
-  }
 
-  return (
-    <div>
-        <h1>{gift || 'NO GIFTS'}</h1>
-        <button onClick = {handleGift} >Lay thuong</button>
-    </div>
+    const handleCheck = (id) => {
+      setCheck(prev => {
+        const isChecked = checked.includes(id)
+        if(isChecked) {
+          return checked.filter(item => item !== id)
+        }else {
+          return [...prev, id]
+        }
+      })
+    }
 
-  )
-  
+    const handleSubmit = () => {  
+        console.log({ids: checked})
+    }
+
+    return (
+        <div style={{padding: 32, color: 'red'}}>
+
+          {courses.map(course => (
+            <div key={course.id}>
+                <input 
+                type="checkbox" 
+                checked= {checked.includes(course.id)}
+                onChange = {() => handleCheck (course.id)}
+                />
+                {course.name}
+            </div>
+          ))}
+
+        <button onClick={handleSubmit} >Register</button>
+        </div>
+    )
 
 }
 
