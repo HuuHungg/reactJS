8f6d1b16378f554a09ebd365cfb77e46cffb2214
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-26 09:50:09 +0700

    add file modifile

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,65 +1,17 @@
-import { toBeInTheDOM } from '@testing-library/jest-dom/dist/matchers'
-import { useState } from 'react'
-
-
-const courses = [
-  {
-    id: 1,
-    name: '日本語',
-  },
-  {
-    id: 2,
-    name: '英語'
-  },
-  {
-    id: 3,
-    name: 'ITを勉強してる'
-  },
-  {
-    id: 4,
-    name: 'ベトナム語'
-  }
-]
-
+import { useState } from 'react';
+import Content from './Content'
 
 function App() {
 
-    const [checked, setCheck] = useState([])
-
-
-    const handleCheck = (id) => {
-      setCheck(prev => {
-        const isChecked = checked.includes(id)
-        if(isChecked) {
-          return checked.filter(item => item !== id)
-        }else {
-          return [...prev, id]
-        }
-      })
-    }
-
-    const handleSubmit = () => {  
-        console.log({ids: checked})
-    }
+    const [show, setShow] = useState(false)
 
     return (
-        <div style={{padding: 32, color: 'red'}}>
-
-          {courses.map(course => (
-            <div key={course.id}>
-                <input 
-                type="checkbox" 
-                checked= {checked.includes(course.id)}
-                onChange = {() => handleCheck (course.id)}
-                />
-                {course.name}
-            </div>
-          ))}
-
-        <button onClick={handleSubmit} >Register</button>
-        </div>
+      <div style={{padding: 20}}>
+          <button onClick={() => setShow(!show)} >Toggle</button>
+          { show && <Content />}
+      </div>
     )
-
+ 
 }
 
 export default App;
