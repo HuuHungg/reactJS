6c143b63dc3dd696dac7226678b831e4b50a2570
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2022-12-28 11:35:11 +0700

    add file useReducer

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,17 +1,52 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import Content from './Content'
 
+// useState 
+// 1. Init state: 0
+// 2. Action: Up (state + 1) / Down (state - 1) 
+
+// use Reducer
+// 1. Init state: 0
+// 2. Action: Up (state + 1) / Down (state - 1) 
+// 3. Reducer
+// 4. Dispatch
+
+// Init state
+const initState = 40
+// Actions
+const UP_ACTION = 'up'
+const DOWN_ACTION = 'down'
+// Reducer
+const reducer = (state, action) => {
+   
+    switch(action) {
+      case UP_ACTION:
+          return state + 4
+      case DOWN_ACTION:
+          return  state - 4
+      default:
+          throw new Error('Invalid action')
+    } 
+}
+
 function App() {
-
-    const [show, setShow] = useState(false)
-
+    const [count, dispatch] = useReducer(reducer, initState)
     return (
-      <div style={{padding: 20}}>
-          <button onClick={() => setShow(!show)} >Toggle</button>
-          { show && <Content />}
+      <div style={{padding: '20px 20px'}}> 
+          <h1>{count}</h1>
+          <button 
+              onClick={() => dispatch(DOWN_ACTION)}
+          >
+              Down
+          </button>
+          <button 
+              onClick={() => dispatch(UP_ACTION)} 
+            >
+                Up
+          </button>
       </div>
     )
- 
 }
+
 
 export default App;
